4d1a633991ae5a3d0333c96c16f6703ae0fde46c
Vikash Hart <vikashhart@gmail.com>
2019-04-07 13:36:48 -0400

    Initial Commit

---
+++ ChromaKeyEffect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChromaKeyEffect
+//
+//  Created by C4Q on 4/7/19.
+//  Copyright © 2019 Vikash Hart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
